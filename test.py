74d1de5c2362c90dfbff766955f351f7de146efd
manoharsuggula <56592645+manoharsuggula@users.noreply.github.com>
2021-11-22 21:57:46 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,22 @@
+import unittest
+
+from factorial import factorial
+
+class TestFactorial(unittest.TestCase):
+    def test_number1(self):
+        data = 6
+        result = factorial(data)
+        self.assertEqual(result, 720)
+
+    def test_number2(self):
+        data = 5
+        result = factorial(data)
+        self.assertEqual(result, 120)
+
+    def test_number3(self):
+        data = 4
+        result = factorial(data)
+        self.assertEqual(result, 24)
+
+if __name__ == '__main__':
+    unittest.main()
